346ba9536479aa346d9ae7a7f42f91cf9d4edab5
Rob Phoenix <rob.phoenix@bt.com>
2016-08-16 17:16:58 +0100

    rewrite script with difflib

--- rddiff.py
+++ rddiff.py
@@ -1,63 +1,47 @@
 import os
 import argparse
 import datetime
-import re
-
+import difflib
 
 CANDIDATE = "./candidate.txt"
-IGNORE = "./ignores.txt"
 
 
 def filelines(fp):
     with open(fp, 'r') as f:
-        return [line.rstrip() for line in f.readlines()]
+        fl = [line.rstrip() for line in f.readlines()]
+    return fl
 
 
-def hostname(fl):
-    fl = filelines(fl)
+def hostname(f):
+    fl = filelines(f)
     hn_lines = list(ln for ln in fl if 'hostname' in ln.lower())
     return hn_lines[0].split()[1]
 
 
-def ignored(ig=IGNORE):
-    ig = filelines(ig)
-    res = [re.compile(pattern) for pattern in ig]
-    return res
-
-
-def normalize(f):
-    f = filelines(f)
-    f = [line for line in f if not line.startswith("!")]
-    for line in f:
-        for i in ignored():
-            if i.match(line):
-                f.remove(line)
-    return f
-
-
-
-def compare(base, case):
-    base = normalize(base)
-    case = normalize(case)
-    missing = [line for line in base if line not in case]
-    extra = [line for line in case if line not in base]
-    res = '[missing]\n\n{0}\n\n[additional]\n\n{1}\n'.format(
-        '\n'.join(missing), '\n'.join(extra))
-    return res
-
-
-def append_to_file(content):
-    now = datetime.datetime.now().isoformat().split(
+now = datetime.datetime.now().isoformat().split(
         '.')[0].replace(':', '').replace('T', '-')
-    f = 'output-{0}.txt'.format(now)
-    with open(f, 'a') as output:
-        output.write(content)
-
-
-for f in os.listdir("./cases"):
-    case = os.path.join("./cases", f)
-    diff = compare(CANDIDATE, case)
-    append_to_file('[{0}] {1}\n\n{2}\n'.format(hostname(case), '*' * 50, diff))
+fout = 'diff-{0}.txt'.format(now)
+with open(fout, 'a') as output:
+    for f in os.listdir("./cases"):
+        case = os.path.join("./cases", f)
+        hn = hostname(case)
+        diff = difflib.context_diff(filelines(CANDIDATE), filelines(case))
+        diff = list(diff)
+        missing = '\n'.join(x[2:] for x in diff if x.startswith('- '))
+        additional = '\n'.join(x[2:] for x in diff if x.startswith('+ '))
+        h = "[Host: {0}]".format(hn)
+        output.write(h + " " + ("*" * (79 - len(h))))
+        print(h + " " + ("*" * (79 - len(h))))
+        output.write("\n-\n")
+        print("\n-\n")
+        output.write(missing)
+        print(missing)
+        output.write("\n+\n")
+        print("\n+\n")
+        output.write(additional)
+        print(additional)
+        output.write("\n\n")
+        print("\n")
 
 
 # def main():
