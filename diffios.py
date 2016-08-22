3353240182f9d90d21df9e3070064f5c1dbead45
Rob Phoenix <robertphoenix@gmail.com>
2016-08-22 12:03:21 +0100

    remove difflib code

--- diffios.py
+++ diffios.py
@@ -34,49 +34,6 @@ def context_list(a):
 
 pprint(context_list("./jon_candidate.conf"))
 
-# def compare(cases=None, candidate=None):
-#     if not cases:
-#         cases = "./cases"
-#     if not candidate:
-#         candidate = "./candidate.txt"
-#     res = "\n{0:=<69} [ Start ]\n".format("")
-#     # d = {}
-#     for f in os.listdir(cases):
-#         d = difflib.Differ(linejunk=lambda x : x == "!")
-#         case = os.path.join(cases, f)
-#         hn = hostname(case)
-#         # d[hn] = {}
-#         out = d.compare(filelines(candidate), filelines(case))
-#         pprint.pprint(list(out))
-#         diff = list(difflib.unified_diff(filelines(candidate), filelines(case), n=0, lineterm="", fromfile=str(candidate), tofile=str(case)))
-#         # for line in diff:
-#         #     print(line)
-#         missing = '\n'.join(x[2:] for x in diff if x.startswith('- '))
-#         additional = '\n'.join(x[2:] for x in diff if x.startswith('+ '))
-#         semi = '\n'.join(x[2:] for x in diff if x.startswith('! '))
-#         # d[hn]["-"] = missing
-#         # d[hn]["+"] = additional
-#         title = "\n[ Host: {0} ] {1:=<{2}}".format(hn, "", (79 - (len(hn) + 11)))
-#         minus = "\n\nminus\n\n{0}".format(missing)
-#         plus = "\n\nplus\n\n{0}".format(additional)
-#         var = "\n\nvar\n\n{0}".format(semi)
-#         res += "{0}{1}{2}{3}\n".format(title, minus, plus, var)
-#     res += "{0:=<68} [ Finish ]\n".format("")
-#     # pprint.pprint(d)
-#     return res
-#
-# def diff_to_file(diff):
-#     timestamp = datetime.datetime.now().isoformat().split(
-#             '.')[0].replace(':', '').replace('T', '-')
-#     fout = 'diff-{0}.txt'.format(timestamp)
-#     with open(fout, 'a') as output:
-#         output.write(diff)
-#     return diff
-#
-# # compare()
-# # diff_to_file(compare(cases="./jon_cases", candidate="./jon_candidate.conf"))
-# compare(cases="./jon_cases", candidate="./jon_candidate.conf")
-
 
 # def main():
 
