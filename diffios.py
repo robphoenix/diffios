9142617214e84c7827267e35524d7f1c3f37a244
Rob Phoenix <robertphoenix@gmail.com>
2016-08-23 13:43:37 +0100

    remove main()

--- diffios.py
+++ diffios.py
@@ -92,34 +92,3 @@ case = context_list("./jon_cases/10.1.240.19.conf")
 
 diff = compare(candidate, case)
 pprint(diff)
-
-# def main():
-
-#     parser = argparse.ArgumentParser(
-#     prog="cscodiff",
-#     description="""
-#         list differences between multiple Cisco configuration files
-#         and a base Cisco configuration file.
-#         """,
-#     formatter_class=argparse.RawDescriptionHelpFormatter,
-#     epilog="""
-#         TODO: add epic description
-#   	""")
-#     parser.add_argument("base_file",
-#                         help="Specify the base configuration file you are comparing against")
-#     parser.add_argument("comparison_files",
-#                         help="Specify the directory containing the Cisco configuration files you want to compare against the base file")
-#     #  group = parser.add_mutually_exclusive_group()
-#     #  group.add_argument("--csv", help="Output results as CSV file", action="store_true")
-#     args = parser.parse_args()
-
-
-#     base = filelines(args.base_file)
-#     for f in os.listdir(args.comparison_files):
-#         case = filelines(os.path.join(args.comparison_files, f))
-#         diff = compare(base, case)
-#         append_to_file('[{0}] {1}\n\n{2}\n'.format(hostname(case), '*' * 50, diff))
-
-
-# if __name__ == '__main__':
-#     main()
