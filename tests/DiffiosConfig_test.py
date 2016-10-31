8574b6618ce64ca8f5868b2553cc047b3816bb97
Rob Phoenix <robertphoenix@gmail.com>
2016-10-31 15:20:49 +0000

    rewrite tests with pytest syntax

--- tests/DiffiosConfig_test.py
+++ tests/DiffiosConfig_test.py
@@ -1,6 +1,7 @@
 import os
 import sys
-import unittest
+
+import pytest
 
 THIS_DIR = os.path.dirname(__file__)
 sys.path.append(os.path.abspath("."))
@@ -9,81 +10,73 @@ from diffios import DiffiosConfig
 from utils import baseline_blocks, baseline_partition
 
 
-class DiffiosConfigTest(unittest.TestCase):
+configs_dir = os.path.abspath(os.path.join("tests", "configs"))
+config = os.path.join(configs_dir, "baseline.conf")
+dc = DiffiosConfig(config)
 
-    def setUp(self):
-        self.configs_dir = os.path.abspath(os.path.join("tests", "configs"))
-        self.config = os.path.join(self.configs_dir, "baseline.conf")
-        self.dc = DiffiosConfig(self.config)
 
-    def test_default_ignore_filename(self):
-        expected = os.path.join(os.getcwd(), "diffios_ignore")
-        actual = self.dc.ignore_filename
-        self.assertEqual(expected, actual)
+def test_default_ignore_filename():
+    expected = os.path.join(os.getcwd(), "diffios_ignore")
+    actual = dc.ignore_filename
+    assert expected == actual
 
-    def test_alternative_ignore_filename(self):
-        alt_ignore_path = os.path.join(THIS_DIR, "alt_diffios_ignore")
-        expected = alt_ignore_path
-        dc = DiffiosConfig(self.config, ignores=alt_ignore_path)
-        actual = dc.ignore_filename
-        self.assertEqual(expected, actual)
 
-    def test_raises_error_if_ignore_file_does_not_exist(self):
-        with self.assertRaises(RuntimeError) as e:
-            DiffiosConfig(self.config, ignores="alt_ignore_file")
-        self.assertEqual(
-            str(e.exception),
-            "[FATAL] DiffiosConfig() received an invalid argument: ignores=alt_ignore_file\n")
+def test_alternative_ignore_filename():
+    alt_ignore_path = os.path.join(THIS_DIR, "alt_diffios_ignore")
+    actual = DiffiosConfig(config, ignores=alt_ignore_path).ignore_filename
+    assert alt_ignore_path == actual
 
-    def test_ignore_filename_is_False_if_ignores_is_False(self):
-        dc = DiffiosConfig(self.config, ignores=False)
-        self.assertFalse(dc.ignore_filename)
 
-    def test_ignore_filename_is_False_if_ignores_is_empty_list(self):
-        dc = DiffiosConfig(self.config, ignores=[])
-        self.assertFalse(dc.ignore_filename)
+def test_raises_error_if_ignore_file_does_not_exist():
+    with pytest.raises(RuntimeError):
+        DiffiosConfig(config, ignores="alt_ignore_file")
 
-    def test_ignore_filename_is_False_if_ignores_is_empty_string(self):
-        dc = DiffiosConfig(self.config, ignores="")
-        self.assertFalse(dc.ignore_filename)
 
-    def test_ignore_filename_is_False_if_ignores_is_zero(self):
-        dc = DiffiosConfig(self.config, ignores=0)
-        self.assertFalse(dc.ignore_filename)
+def test_ignore_filename_is_False_if_ignores_is_False():
+    assert DiffiosConfig(config, ignores=False).ignore_filename is False
 
-    def test_config_filename(self):
-        actual = self.config
-        expected = self.dc.config_filename
-        self.assertEqual(expected, actual)
 
-    # config property now removes invalid lines
-    # def test_config(self):
-        # expected = [l.rstrip() for l in open(self.config).readlines()]
-        # actual = self.df.config
-        # self.assertEqual(expected, actual)
+def test_ignore_filename_is_False_if_ignores_is_empty_list():
+    assert DiffiosConfig(config, ignores=[]).ignore_filename is False
 
-    def test_hostname(self):
-        expected = "BASELINE01"
-        actual = self.dc.hostname
-        self.assertEqual(expected, actual)
 
-    def test_blocks(self):
-        expected = baseline_blocks()
-        actual = self.dc.config_blocks
-        self.assertEqual(expected, actual)
+def test_ignore_filename_is_False_if_ignores_is_empty_string():
+    assert DiffiosConfig(config, ignores="").ignore_filename is False
 
-    def test_ignore_lines(self):
-        ignore_file = open("diffios_ignore").readlines()
-        expected = [l.strip().lower() for l in ignore_file]
-        actual = self.dc.ignores
-        self.assertEqual(expected, actual)
 
-    def test_ignored(self):
-        expected = baseline_partition().ignored
-        actual = self.dc.ignored
-        self.assertEqual(expected, actual)
+def test_ignore_filename_is_False_if_ignores_is_zero():
+    assert DiffiosConfig(config, ignores=0).ignore_filename is False
 
-    def test_recorded(self):
-        expected = baseline_partition().recorded
-        actual = self.dc.recorded
-        self.assertEqual(expected, actual)
+
+def test_config_filename():
+    assert dc.config_filename == config
+
+
+def test_config():
+    pytest.skip('config property now removes invalid lines')
+    expected = [l.rstrip() for l in open(config).readlines()]
+    assert expected, dc.config
+
+
+def test_hostname():
+    assert "BASELINE01" == dc.hostname
+
+
+def test_blocks():
+    assert baseline_blocks() == dc.config_blocks
+
+
+def test_ignore_lines():
+    ignore_file = open("diffios_ignore").readlines()
+    expected = [l.strip().lower() for l in ignore_file]
+    assert expected == dc.ignores
+
+
+def test_ignored():
+    expected = baseline_partition().ignored
+    assert expected == dc.ignored
+
+
+def test_recorded():
+    expected = baseline_partition().recorded
+    assert expected == dc.recorded
--- tests/DiffiosDiff_test.py
+++ tests/DiffiosDiff_test.py
@@ -1,6 +1,5 @@
 import os
 import sys
-from unittest import TestCase
 
 sys.path.append(os.path.abspath("."))
 from diffios import DiffiosDiff
@@ -142,82 +141,75 @@ authentication mac-move permit
 authentication critical recovery delay 600""".split('\n')
 
 
-class DiffiosDiffTest(TestCase):
+def test_different_vlan_interface_config():
+    baseline = [
+        'hostname BASELINE',
+        'interface Vlan1',
+        ' no ip address',
+        ' shutdown'
+    ]
+    comparison = [
+        'hostname COMPARISON',
+        'interface Vlan1',
+        ' ip address 192.168.0.1 255.255.255.0',
+        ' no shutdown'
+    ]
+    expected_additional = [[
+        'interface Vlan1',
+        ' ip address 192.168.0.1 255.255.255.0',
+        ' no shutdown'
+    ]]
+    expected_missing = [[
+        'interface Vlan1',
+        ' no ip address',
+        ' shutdown'
+    ]]
+    diff = DiffiosDiff(baseline=baseline, comparison=comparison)
+    assert expected_additional == diff.additional
+    assert expected_missing == diff.missing
 
-    def test_different_vlan_interface_config(self):
-        baseline = [
-            'hostname BASELINE',
-            'interface Vlan1',
-            ' no ip address',
-            ' shutdown'
-        ]
-        comparison = [
-            'hostname COMPARISON',
-            'interface Vlan1',
-            ' ip address 192.168.0.1 255.255.255.0',
-            ' no shutdown'
-        ]
-        expected_additional = [[
-            'interface Vlan1',
-            ' ip address 192.168.0.1 255.255.255.0',
-            ' no shutdown'
-        ]]
-        expected_missing = [[
-            'interface Vlan1',
-            ' no ip address',
-            ' shutdown'
-        ]]
-        diff = DiffiosDiff(baseline=baseline, comparison=comparison)
-        actual_additional = diff.additional
-        actual_missing = diff.missing
-        self.assertEqual(expected_additional, actual_additional)
-        self.assertEqual(expected_missing, actual_missing)
 
-    def test_different_fast_interface_config_ignoring_description(self):
-        expected_additional = [[
-            'interface FastEthernet0/5',
-            ' switchport trunk native vlan 999',
-            ' switchport trunk allowed vlan 510,511,999',
-            ' switchport mode trunk'
-        ]]
-        expected_missing = [[
-            'interface FastEthernet0/5',
-            ' switchport mode access'
-        ]]
-        diff = DiffiosDiff(baseline=int_baseline, comparison=int_comparison)
-        actual_additional = diff.additional
-        actual_missing = diff.missing
-        self.assertEqual(expected_additional, actual_additional)
-        self.assertEqual(expected_missing, actual_missing)
+def test_different_fast_interface_config_ignoring_description():
+    expected_additional = [[
+        'interface FastEthernet0/5',
+        ' switchport trunk native vlan 999',
+        ' switchport trunk allowed vlan 510,511,999',
+        ' switchport mode trunk'
+    ]]
+    expected_missing = [[
+        'interface FastEthernet0/5',
+        ' switchport mode access'
+    ]]
+    diff = DiffiosDiff(baseline=int_baseline, comparison=int_comparison)
+    assert expected_additional == diff.additional
+    assert expected_missing == diff.missing
 
-    def test_different_aaa_config(self):
-        expected_additional = [
-            ['aaa accounting commands 0 CON start-stop group tacacs+'],
-            ['aaa accounting commands 0 VTY start-stop group tacacs+'],
-            ['aaa accounting commands 15 CON start-stop group tacacs+'],
-            ['aaa accounting commands 15 VTY start-stop group tacacs+'],
-            ['aaa accounting exec CON start-stop group tacacs+'],
-            ['aaa accounting exec VTY start-stop group tacacs+'],
-            ['aaa authentication login CON group tacacs+ local'],
-            ['aaa authentication login VTY group tacacs+ local'],
-            ['aaa authentication login default group tacacs+ local'],
-            ['aaa authorization exec CON group tacacs+ local'],
-            ['aaa authorization exec VTY group tacacs+ local'],
-            ['aaa authorization exec default group tacacs+ local'],
-            ['aaa server radius dynamic-author',
-             ' client 10.10.20.1 server-key 7 1234567890ABCDEFGHIJKL']]
-        expected_missing = [
-            ['aaa accounting exec CON start-stop group radius'],
-            ['aaa accounting exec VTY start-stop group radius'],
-            ['aaa authentication login CON group radius local'],
-            ['aaa authentication login VTY group radius local'],
-            ['aaa authorization exec CON group radius local'],
-            ['aaa authorization exec VTY group radius local'],
-            ['aaa server radius dynamic-author',
-             ' client 10.10.21.1 server-key 7 1234567890ABCDEFGHIJKL']]
-        diff = DiffiosDiff(baseline=aaa_baseline, comparison=aaa_comparison)
-        actual_additional = diff.additional
-        print(actual_additional)
-        actual_missing = diff.missing
-        self.assertEqual(expected_additional, actual_additional)
-        self.assertEqual(expected_missing, actual_missing)
+
+def test_different_aaa_config():
+    expected_additional = [
+        ['aaa accounting commands 0 CON start-stop group tacacs+'],
+        ['aaa accounting commands 0 VTY start-stop group tacacs+'],
+        ['aaa accounting commands 15 CON start-stop group tacacs+'],
+        ['aaa accounting commands 15 VTY start-stop group tacacs+'],
+        ['aaa accounting exec CON start-stop group tacacs+'],
+        ['aaa accounting exec VTY start-stop group tacacs+'],
+        ['aaa authentication login CON group tacacs+ local'],
+        ['aaa authentication login VTY group tacacs+ local'],
+        ['aaa authentication login default group tacacs+ local'],
+        ['aaa authorization exec CON group tacacs+ local'],
+        ['aaa authorization exec VTY group tacacs+ local'],
+        ['aaa authorization exec default group tacacs+ local'],
+        ['aaa server radius dynamic-author',
+            ' client 10.10.20.1 server-key 7 1234567890ABCDEFGHIJKL']]
+    expected_missing = [
+        ['aaa accounting exec CON start-stop group radius'],
+        ['aaa accounting exec VTY start-stop group radius'],
+        ['aaa authentication login CON group radius local'],
+        ['aaa authentication login VTY group radius local'],
+        ['aaa authorization exec CON group radius local'],
+        ['aaa authorization exec VTY group radius local'],
+        ['aaa server radius dynamic-author',
+            ' client 10.10.21.1 server-key 7 1234567890ABCDEFGHIJKL']]
+    diff = DiffiosDiff(baseline=aaa_baseline, comparison=aaa_comparison)
+    assert expected_additional == diff.additional
+    assert expected_missing == diff.missing
