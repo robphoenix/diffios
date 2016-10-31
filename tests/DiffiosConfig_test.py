82159bb86ff1f945582d42a0b02745c9f004ad10
Rob Phoenix <robertphoenix@gmail.com>
2016-10-31 16:13:10 +0000

    make use of pytest.fixture

--- tests/DiffiosConfig_test.py
+++ tests/DiffiosConfig_test.py
@@ -7,76 +7,81 @@ THIS_DIR = os.path.dirname(__file__)
 sys.path.append(os.path.abspath("."))
 
 from diffios import DiffiosConfig
-from utils import baseline_blocks, baseline_partition
 
 
-configs_dir = os.path.abspath(os.path.join("tests", "configs"))
-config = os.path.join(configs_dir, "baseline.conf")
-dc = DiffiosConfig(config)
+@pytest.fixture
+def config():
+    configs_dir = os.path.abspath(os.path.join("tests", "configs"))
+    return os.path.join(configs_dir, "baseline.conf")
 
 
-def test_default_ignore_filename():
+@pytest.fixture
+def dc():
+    return DiffiosConfig(config())
+
+
+def test_default_ignore_filename(dc):
     expected = os.path.join(os.getcwd(), "diffios_ignore")
     actual = dc.ignore_filename
     assert expected == actual
 
 
-def test_alternative_ignore_filename():
+def test_alternative_ignore_filename(config):
     alt_ignore_path = os.path.join(THIS_DIR, "alt_diffios_ignore")
     actual = DiffiosConfig(config, ignores=alt_ignore_path).ignore_filename
     assert alt_ignore_path == actual
 
 
-def test_raises_error_if_ignore_file_does_not_exist():
+def test_raises_error_if_ignore_file_does_not_exist(config):
     with pytest.raises(RuntimeError):
         DiffiosConfig(config, ignores="alt_ignore_file")
 
 
-def test_ignore_filename_is_False_if_ignores_is_False():
+def test_ignore_filename_is_False_if_ignores_is_False(config):
     assert DiffiosConfig(config, ignores=False).ignore_filename is False
 
 
-def test_ignore_filename_is_False_if_ignores_is_empty_list():
+def test_ignore_filename_is_False_if_ignores_is_empty_list(config):
     assert DiffiosConfig(config, ignores=[]).ignore_filename is False
 
 
-def test_ignore_filename_is_False_if_ignores_is_empty_string():
+def test_ignore_filename_is_False_if_ignores_is_empty_string(config):
     assert DiffiosConfig(config, ignores="").ignore_filename is False
 
 
-def test_ignore_filename_is_False_if_ignores_is_zero():
+def test_ignore_filename_is_False_if_ignores_is_zero(config):
     assert DiffiosConfig(config, ignores=0).ignore_filename is False
 
 
-def test_config_filename():
+def test_config_filename(dc, config):
     assert dc.config_filename == config
 
 
-def test_config():
+def test_config(dc, config):
     pytest.skip('config property now removes invalid lines')
     expected = [l.rstrip() for l in open(config).readlines()]
     assert expected, dc.config
 
 
-def test_hostname():
+def test_hostname(dc):
     assert "BASELINE01" == dc.hostname
 
 
-def test_blocks():
-    assert baseline_blocks() == dc.config_blocks
+def test_blocks(dc, baseline_blocks):
+    assert baseline_blocks == dc.config_blocks
 
 
-def test_ignore_lines():
+def test_ignore_lines(dc):
     ignore_file = open("diffios_ignore").readlines()
     expected = [l.strip().lower() for l in ignore_file]
     assert expected == dc.ignores
 
 
-def test_ignored():
-    expected = baseline_partition().ignored
+def test_ignored(dc, baseline_partition):
+    expected = baseline_partition.ignored
     assert expected == dc.ignored
 
 
-def test_recorded():
-    expected = baseline_partition().recorded
+def test_recorded(dc, baseline_partition):
+    expected = baseline_partition.recorded
     assert expected == dc.recorded
--- tests/DiffiosDiff_test.py
+++ tests/DiffiosDiff_test.py
@@ -4,142 +4,6 @@ import sys
 sys.path.append(os.path.abspath("."))
 from diffios import DiffiosDiff
 
-int_baseline = """interface FastEthernet0/5
- description Data and Voice Access Port
- switchport access vlan 101
- switchport mode access
- switchport nonegotiate
- ip access-group ALLOW-ALL in
- no logging event link-status
- srr-queue bandwidth share 1 30 35 5
- priority-queue out
- authentication event fail action next-method
- authentication event server dead action authorize
- authentication event server dead action authorize voice
- authentication event server alive action reinitialize
- authentication host-mode multi-auth
- authentication open
- authentication order dot1x mab
- authentication priority dot1x mab
- authentication port-control auto
- authentication periodic
- authentication timer reauthenticate server
- authentication violation restrict
- mab
- snmp trap mac-notification change added
- no snmp trap link-status
- mls qos trust dscp
- dot1x pae authenticator
- dot1x timeout tx-period 10
- storm-control broadcast level 20.00
- spanning-tree portfast
- spanning-tree bpduguard enable
- service-policy input QOS-CLASSIFY""".split('\n')
-
-int_comparison = """interface FastEthernet0/5
- description Cisco AP
- switchport access vlan 101
- switchport trunk native vlan 999
- switchport trunk allowed vlan 510,511,999
- switchport mode trunk
- switchport nonegotiate
- ip access-group ALLOW-ALL in
- no logging event link-status
- srr-queue bandwidth share 1 30 35 5
- priority-queue out
- authentication event fail action next-method
- authentication event server dead action authorize
- authentication event server dead action authorize voice
- authentication event server alive action reinitialize
- authentication host-mode multi-auth
- authentication open
- authentication order dot1x mab
- authentication priority dot1x mab
- authentication port-control auto
- authentication periodic
- authentication timer reauthenticate server
- authentication violation restrict
- mab
- snmp trap mac-notification change added
- no snmp trap link-status
- mls qos trust dscp
- dot1x pae authenticator
- dot1x timeout tx-period 10
- storm-control broadcast level 20.00
- spanning-tree portfast
- spanning-tree bpduguard enable
- service-policy input QOS-CLASSIFY""".split('\n')
-
-aaa_baseline = """aaa group server radius ISE
- server name bob
- server name alice
- load-balance method least-outstanding
-!
-aaa authentication login VTY group radius local
-aaa authentication login CON group radius local
-aaa authentication dot1x default group radius
-aaa authorization console
-aaa authorization config-commands
-aaa authorization exec VTY group radius local
-aaa authorization exec CON group radius local
-aaa authorization network default group radius
-aaa accounting update periodic 15
-aaa accounting dot1x default start-stop group radius
-aaa accounting exec VTY start-stop group radius
-aaa accounting exec CON start-stop group radius
-aaa accounting system default start-stop group radius
-no aaa accounting system guarantee-first
-!
-aaa server radius dynamic-author
- client 10.10.21.1 server-key 7 1234567890ABCDEFGHIJKL
- client 10.20.20.1 server-key 7 1234567890ABCDEFGHIJKL
-!
-aaa session-id common
-clock summer-time BST recurring last Sun Mar 2:00 last Sun Oct 2:00
-system mtu routing 1500
-authentication mac-move permit
-authentication critical recovery delay 600""".split('\n')
-
-aaa_comparison = """aaa group server radius ISE
- server name bob
- server name alice
- load-balance method least-outstanding
-!
-aaa authentication login default group tacacs+ local
-aaa authentication login VTY group tacacs+ local
-aaa authentication login CON group tacacs+ local
-aaa authentication dot1x default group radius
-aaa authorization console
-aaa authorization config-commands
-aaa authorization exec default group tacacs+ local
-aaa authorization exec VTY group tacacs+ local
-aaa authorization exec CON group tacacs+ local
-aaa authorization network default group radius
-aaa accounting update periodic 15
-aaa accounting dot1x default start-stop group radius
-aaa accounting exec VTY start-stop group tacacs+
-aaa accounting exec CON start-stop group tacacs+
-aaa accounting commands 0 VTY start-stop group tacacs+
-aaa accounting commands 0 CON start-stop group tacacs+
-aaa accounting commands 15 VTY start-stop group tacacs+
-aaa accounting commands 15 CON start-stop group tacacs+
-aaa accounting system default start-stop group radius
-no aaa accounting system guarantee-first
-!
-!
-!
-!
-!
-aaa server radius dynamic-author
- client 10.10.20.1 server-key 7 1234567890ABCDEFGHIJKL
- client 10.20.20.1 server-key 7 1234567890ABCDEFGHIJKL
-!
-aaa session-id common
-clock summer-time BST recurring last Sun Mar 2:00 last Sun Oct 2:00
-system mtu routing 1500
-authentication mac-move permit
-authentication critical recovery delay 600""".split('\n')
-
 
 def test_different_vlan_interface_config():
     baseline = [
@@ -169,7 +33,7 @@ def test_different_vlan_interface_config():
     assert expected_missing == diff.missing
 
 
-def test_different_fast_interface_config_ignoring_description():
+def test_different_fast_interface_config_ignoring_description(int_baseline, int_comparison):
     expected_additional = [[
         'interface FastEthernet0/5',
         ' switchport trunk native vlan 999',
@@ -185,7 +49,7 @@ def test_different_fast_interface_config_ignoring_description():
     assert expected_missing == diff.missing
 
 
-def test_different_aaa_config():
+def test_different_aaa_config(aaa_baseline, aaa_comparison):
     expected_additional = [
         ['aaa accounting commands 0 CON start-stop group tacacs+'],
         ['aaa accounting commands 0 VTY start-stop group tacacs+'],
