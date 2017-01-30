84bd05297175e8f60ba9f80687813f250a059ed3
Rob Phoenix <robphoenix@protonmail.com>
2017-01-30 17:32:17 +0000

    update run_diffios.py

--- run_diffios.py
+++ run_diffios.py
@@ -4,7 +4,7 @@
 import os
 import csv
 
-from diffios import Diffios
+import diffios
 
 IGNORE_FILE = os.path.join(os.getcwd(), "priv", "diffios_ignore")
 COMPARISON_DIR = os.path.join(os.getcwd(), "priv", "anchor")
@@ -16,10 +16,11 @@ with open(os.path.join(os.getcwd(), "priv", "diffs.csv"), 'w') as csvfile:
     csvwriter = csv.writer(csvfile, lineterminator='\n')
     csvwriter.writerow(["Comparison", "Baseline", "Additional", "Missing"])
     files = sorted(os.listdir(COMPARISON_DIR))
-    for fin in files:
-        print("Processing: ", fin)
+    num_files = len(files)
+    for i, fin in enumerate(files):
+        print("diffios: {:>3}/{} Processing: {:>20}".format(i, num_files, fin), end="\r")
         comparison_file = os.path.join(COMPARISON_DIR, fin)
-        diff = Diffios(BASELINE_FILE, comparison_file, IGNORE_FILE)
+        diff = diffios.Compare(BASELINE_FILE, comparison_file, IGNORE_FILE)
         csvwriter.writerow([
             fin,
             os.path.basename(BASELINE_FILE),
