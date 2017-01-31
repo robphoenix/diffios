e8a37985d421b1f3aad340fe2fb8d9cdbce27c87
Rob Phoenix <robphoenix@protonmail.com>
2017-01-31 12:46:47 +0000

    move run_diffios to examples dir

--- examples/run_diffios.py
+++ examples/run_diffios.py
@@ -3,22 +3,26 @@
 
 import os
 import csv
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import diffios
 
-IGNORE_FILE = os.path.join(os.getcwd(), "priv", "diffios_ignore")
-COMPARISON_DIR = os.path.join(os.getcwd(), "priv", "anchor")
-BASELINE_FILENAME = "sm_sw_baseline.txt"
+IGNORE_FILE = os.path.join(os.getcwd(), "ignores.txt")
+COMPARISON_DIR = os.path.join(os.getcwd(), "configs", "comparisons")
 BASELINE_FILE = os.path.join(
-    os.getcwd(), "priv", "anchor_baselines", BASELINE_FILENAME)
+    os.getcwd(), "configs", "baselines", "baseline.txt")
 
-with open(os.path.join(os.getcwd(), "priv", "diffs.csv"), 'w') as csvfile:
+output = os.path.join(os.getcwd(), "diffs.csv")
+
+with open(output, 'w') as csvfile:
     csvwriter = csv.writer(csvfile, lineterminator='\n')
     csvwriter.writerow(["Comparison", "Baseline", "Additional", "Missing"])
     files = sorted(os.listdir(COMPARISON_DIR))
     num_files = len(files)
     for i, fin in enumerate(files):
-        print("diffios: {:>3}/{} Processing: {:>20}".format(i, num_files, fin), end="\r")
+        #  print("diffios: {:>3}/{} Processing: {}".format(i, num_files, fin),
+        #        end="\r")
         comparison_file = os.path.join(COMPARISON_DIR, fin)
         diff = diffios.Compare(BASELINE_FILE, comparison_file, IGNORE_FILE)
         csvwriter.writerow([
@@ -27,3 +31,5 @@ with open(os.path.join(os.getcwd(), "priv", "diffs.csv"), 'w') as csvfile:
             diff.pprint_additional(),
             diff.pprint_missing()
         ])
+        print(diff.delta())
+    print("diffios: Report: {}".format(output))
