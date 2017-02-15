eb98d2a5ae37c1966c887c4f6419e204f9b5d85e
Rob Phoenix <robphoenix@protonmail.com>
2017-02-15 16:44:48 +0000

    update badges

--- setup.py
+++ setup.py
@@ -1,36 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open('README.md') as f:
     long_description = f.read()
-
-
-with open(path.join(here, 'LICENSE'), encoding='utf-8') as f:
-    license = f.read()
 
 setup(
     name='diffios',
-    version='0.0.2',
+    version='0.0.4',
     description='Compare Cisco device configurations against a baseline.',
     long_description=long_description,
     author='Rob Phoenix',
     author_email='rob@robphoenix.com',
+    license='MIT',
     url='https://github.com/robphoenix/diffios',
-    license=license,
     classifiers=[
         'Development Status :: 4 - Beta',
-
-        'Intended Audience :: Network Engineers',
-        'Topic :: Network Automation :: Configuration Management',
-
-        'License :: OSI Approved :: MIT License',
-
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
@@ -38,7 +24,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
     ],
-    keywords='cisco configuration management',
     packages=find_packages(exclude=('tests', 'docs'))
 )
